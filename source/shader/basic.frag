e5d7265b867051f59aaeb2a6966c790ec5d8bd8e
Anton Zakharov <antonzhome@gmail.com>
2018-12-25 11:15:17 -0500

    added state

---
+++ source/shader/basic.frag
@@ -0,0 +1,26 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 Position;
+
+uniform vec2 mouse;
+uniform float time;
+
+const int MAX_ITER = 100;
+
+float mandelbrot(vec2 c) {
+  vec2 z = vec2(c);
+  for (float i = 0.0; i < MAX_ITER; i++) {
+    if (abs(dot(z, z)) > 2.0) {
+      return (i / MAX_ITER);
+    }
+    z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+  }
+  return 1.0;
+}
+
+void main() {
+  float result = mandelbrot(Position + mouse);
+  FragColor = vec4(result, result, result, 1.0);
+}
---
+++ source/state/state.cpp
@@ -0,0 +1,40 @@
+#include "state.h"
+
+#include "../config.h"
+#include "../world.h"
+#include "../player.h"
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+State::State(const Config &config, GLFWwindow *w)
+: mWorld(config), mPlayer(config), window(w) {
+}
+
+void State::handleInput() {
+  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT))
+    mPlayer.handleMouse(window);
+  else
+    mPlayer.stopPressing();
+
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    mPlayer.handleKeyboard(FORWARD);
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    mPlayer.handleKeyboard(BACKWARD);
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    mPlayer.handleKeyboard(LEFT);
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    mPlayer.handleKeyboard(RIGHT);
+}
+
+void State::update() {
+
+}
+
+void State::render() {
+  mWorld.render(mPlayer.getProjectionMatrix(), mPlayer.getViewMatrix(), mPlayer.getPos());
+}
